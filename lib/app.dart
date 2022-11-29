1149a93d829ff04183ece1fd9d4ce6e3667cc8b9
Lukáš Hermann <tuxilero@gmail.com>
2022-11-29 10:21:00 +0100

    Project cleanup

---
+++ lib/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:xkcd/feature_home/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
